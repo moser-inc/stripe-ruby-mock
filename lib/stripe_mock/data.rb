570c98fd9c748f420f69ebb9a64026d6d18ae601
Greg Woods <greg.woods@moserit.com>
2020-05-29 11:22:41 -0400

    Add missing data points for subscriptions: add_invoice_items, latest_invoice

--- lib/stripe_mock/data.rb
+++ lib/stripe_mock/data.rb
@@ -325,6 +325,7 @@ module StripeMock
         current_period_start: 1308595038,
         current_period_end: 1308681468,
         status: 'trialing',
+        latest_invoice: 'in_test_invoice',
         trial_from_plan: false,
         plan: {
           interval: 'month',
